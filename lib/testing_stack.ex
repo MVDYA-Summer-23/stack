fdb558030a855dcf80727a7d71f9eff6d9997f32
Mario Vellandi <mvellandi@hotmail.com>
2023-07-13 23:09:16 +0200

    added functionality with passing tests

--- lib/testing_stack.ex
+++ lib/testing_stack.ex
@@ -1,18 +1,28 @@
 defmodule TestingStack do
-  @moduledoc """
-  Documentation for `TestingStack`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link(init_state, _opts \\ []) do
+    GenServer.start_link(__MODULE__, init_state)
+    # GenServer.start_link(module, init_arg, options \\ [])
+  end
 
-  ## Examples
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
 
-      iex> TestingStack.hello()
-      :world
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
 
-  """
-  def hello do
-    :world
+  def init(state) do
+    {:ok, state}
+  end
+
+  def handle_call(:pop, _from, [] = state), do: {:reply, [], state}
+  def handle_call(:pop, _from, [first | remaining]), do: {:reply, first, remaining}
+
+  def handle_cast({:push, element}, state) do
+    new_state = [element | state]
+    {:noreply, new_state}
   end
 end
